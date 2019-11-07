041f475fdcfb9061207972fefe034f1377ef09db
zs <zs@bjpowernode.com>
2019-11-07 21:51:59 +0800

    增加了输出信息

---
+++ code/spring02/02-spring-mvc/src/main/java/com/bjpowernode/web/controller/MyController.java
@@ -0,0 +1,29 @@
+package com.bjpowernode.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Author:张京远
+ * 2019/11/7
+ */
+@Controller
+public class MyController {
+
+    @RequestMapping(value="/student/save.do",method= RequestMethod.GET)
+    public ModelAndView doSome(String name,Integer age){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("myname",name);
+        mv.addObject("myage",age);
+        mv.setViewName("result");
+        System.out.println(123);
+        return mv;
+    }
+
+    @RequestMapping(value="/student/turnTo.do")
+    public String turnTo(){
+        return "result2";
+    }
+}
